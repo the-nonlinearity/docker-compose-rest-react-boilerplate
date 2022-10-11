e59461682e313693bb8a0901e5a7a2ab457e8be8
BumbleGoose <i.r.r.laurie@gmail.com>
2022-10-11 11:30:33 +0100

    remove unnecessary imports

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,7 +1,6 @@
 import React, { FormEvent, useContext, useEffect, useState } from 'react'
 import { Button, Spinner, Form, Container } from 'react-bootstrap'
 import { toast } from 'react-toastify'
-// import { toast } from 'react-toastify'
 
 import { AuthContext } from '../../context/AuthContext'
 
@@ -48,21 +47,21 @@ export function Login () {
   return (
     <div>
       <h2 className='text-center'>Login</h2>
-      <Container className="bg-light border border-dark border-3">
+    <Container className="bg-light border border-dark border-3">
 
-        <Form data-testid="login-form" onSubmit={handleSubmit}>
+      <Form data-testid="login-form" onSubmit={handleSubmit}>
 
-          <Form.Group className='mb-3' controlId='formEmailLogin'>
+        <Form.Group className='mb-3' controlId='formEmailLogin'>
           <Form.Label>Email:</Form.Label>
           <Form.Control type="email" placeholder="Enter email" onChange={handleChange} name="email" value={values.email} disabled={loginRequestStatus === 'loading'} required />
+        </Form.Group>
 
-          </Form.Group>
-          <Form.Group className='mb-3' controlId='formPasswordLogin'>
+        <Form.Group className='mb-3' controlId='formPasswordLogin'>
           <Form.Label>Password:</Form.Label>
           <Form.Control type="password" placeholder="Enter password" onChange={handleChange} name="password" value={values.password} disabled={loginRequestStatus === 'loading'} required />
         </Form.Group>
 
-          <Container className="align-content-center text-center justify-content-center mb-2 pb-2">
+      <Container className="align-content-center text-center justify-content-center mb-2 pb-2">
 
         {loginRequestStatus !== 'loading'
           ? <Button
@@ -82,11 +81,11 @@ export function Login () {
           <span className="visually-hidden">Loading...</span>
         </Button>
         }
-          </Container>
-
-        </Form>
-
       </Container>
+
+      </Form>
+
+    </Container>
     </div>
   )
 }
